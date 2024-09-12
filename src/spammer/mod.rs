0e380c031da4482f9965fcd12070094398fe5514
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-09-12 11:52:33 -0700

    fix sub-ms intervals; enable >1000 tps

--- src/spammer/mod.rs
+++ src/spammer/mod.rs
@@ -34,7 +34,7 @@ where
     /// Send transactions to the RPC at a given rate. Actual rate may vary; this is only the attempted sending rate.
     pub fn spam_rpc(&self, tx_per_second: usize, duration: usize) -> Result<()> {
         let tx_requests = self.generator.get_spam_txs(tx_per_second * duration)?;
-        let interval = std::time::Duration::from_millis(1_000 / tx_per_second as u64);
+        let interval = std::time::Duration::from_nanos(1_000_000_000 / tx_per_second as u64);
 
         for tx in tx_requests {
             // clone Arc
