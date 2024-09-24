0d6f7bc66a58df4cc41a40dc266284b69c10858d
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-09-24 11:46:52 -0700

    update callback params

--- src/spammer/util.rs
+++ src/spammer/util.rs
@@ -2,7 +2,7 @@
 pub mod test {
     use std::collections::HashMap;
 
-    use alloy::primitives::TxHash;
+    use alloy::providers::PendingTransactionConfig;
     use tokio::task::JoinHandle;
 
     use crate::{generator::NamedTxRequest, spammer::OnTxSent};
@@ -11,11 +11,11 @@ pub mod test {
     impl OnTxSent<String> for MockCallback {
         fn on_tx_sent(
             &self,
-            _tx_hash: TxHash,
+            _tx_res: PendingTransactionConfig,
             _req: NamedTxRequest,
             _extra: Option<HashMap<String, String>>,
         ) -> Option<JoinHandle<()>> {
-            println!("MockCallback::on_tx_sent: tx_hash={}", _tx_hash);
+            println!("MockCallback::on_tx_sent: tx_hash={}", _tx_res.tx_hash());
             None
         }
     }
