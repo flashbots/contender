9a28a22102899b1e28c6c2a55be2e56ac8d4a242
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-09-26 14:46:16 -0700

    use canonical weth, compile separate solc versions per file

---
+++ contracts/src/_sol5.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.5.0;
+
+/* 
+    This file imports contracts solely for the sake of compilation.
+*/
+
+import "lib/canonical-weth/contracts/WETH9.sol";
+import "lib/v2-core/contracts/UniswapV2Factory.sol";
---
+++ contracts/src/_sol6.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+/* 
+    This file imports contracts solely for the sake of compilation.
+*/
+
+import "lib/v2-periphery/contracts/UniswapV2Router02.sol";
