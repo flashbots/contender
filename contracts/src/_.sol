ffd9fc40451c06afa359d959b51297d877672145
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-09-26 14:12:17 -0700

    add WETH & SpamMe

---
+++ contracts/src/_.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/* 
+    This file imports contracts solely for the sake of compilation.
+*/
+
+import "lib/high-volume-contract/src/SpamMe.sol";
