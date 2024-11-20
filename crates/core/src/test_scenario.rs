9c4ae961fd9262e5f8f85935cb78a8bd8ce412b3
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2024-11-19 23:46:19 -0800

    remove unused varc

--- crates/core/src/test_scenario.rs
+++ crates/core/src/test_scenario.rs
@@ -415,7 +415,6 @@ where
         let payloads = payloads.to_owned();
 
         let mut tasks: Vec<tokio::task::JoinHandle<()>> = vec![];
-        let num_iterations = payloads.len();
 
         for payload in payloads {
             let rpc_client = self.rpc_client.clone();
