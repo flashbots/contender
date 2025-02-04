c6863a212ad1045288c265bda210714bc3db0d07
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-02-03 16:16:42 -0800

    remove erroneous parenthesis-removal, replace bad error types in generator::util

--- crates/core/src/generator/util.rs
+++ crates/core/src/generator/util.rs
@@ -17,25 +17,16 @@ use alloy::{
 /// assert_eq!(calldata.encode_hex(), "60fe47b10000000000000000000000000000000000000000000000000000000012345678");
 /// ```
 pub fn encode_calldata(args: &[impl AsRef<str>], sig: &str) -> Result<Vec<u8>> {
-    // Removing inner parenthesis flattens the signature,
-    // so we can support signatures that use nested tuples.
-    //   e.g. "set((uint256, uint256), uint256)" -> "set(uint256, uint256, uint256)"
-    // Alloy doesn't natively support tuples in fn signatures,
-    // but we can flatten them to achieve the same effect.
-    // Args just have to be passed in flat form.
-    let sig = remove_inner_parentheses(sig);
-    let func = json_abi::Function::parse(&sig).map_err(|e| {
-        ContenderError::SpamError("failed to parse function name", Some(e.to_string()))
-    })?;
+    let func = json_abi::Function::parse(&sig)
+        .map_err(|e| ContenderError::with_err(e, "failed to parse function signature"))?;
     let values: Vec<DynSolValue> = args
         .iter()
         .enumerate()
         .map(|(idx, arg)| {
             let mut argtype = String::new();
             func.inputs[idx].full_selector_type_raw(&mut argtype);
-            let r#type = DynSolType::parse(&argtype).map_err(|e| {
-                ContenderError::SpamError("failed to parse function type", Some(e.to_string()))
-            })?;
+            let r#type = DynSolType::parse(&argtype)
+                .map_err(|e| ContenderError::with_err(e, "failed to parse function type"))?;
             r#type.coerce_str(arg.as_ref()).map_err(|e| {
                 ContenderError::SpamError(
                     "failed to coerce arg to DynSolValue",
@@ -44,63 +35,17 @@ pub fn encode_calldata(args: &[impl AsRef<str>], sig: &str) -> Result<Vec<u8>> {
             })
         })
         .collect::<Result<_>>()?;
-    let input = func.abi_encode_input(&values).map_err(|e| {
-        ContenderError::SpamError("failed to encode function arguments", Some(e.to_string()))
-    })?;
+    let input = func
+        .abi_encode_input(&values)
+        .map_err(|e| ContenderError::with_err(e, "failed to encode function arguments"))?;
     Ok(input)
-}
-
-/// Removes inner parentheses from a string.
-pub fn remove_inner_parentheses(input: &str) -> String {
-    let mut result = String::new();
-    let mut depth = 0;
-
-    for c in input.chars() {
-        match c {
-            '(' => {
-                if depth == 0 {
-                    result.push(c); // Keep the outermost opening parenthesis
-                }
-                depth += 1;
-            }
-            ')' => {
-                depth -= 1;
-                if depth == 0 {
-                    result.push(c); // Keep the outermost closing parenthesis
-                }
-            }
-            _ => {
-                result.push(c); // Always keep the content inside parentheses
-            }
-        }
-    }
-
-    result
 }
 
 #[cfg(test)]
 pub mod test {
-    use super::*;
     use alloy::node_bindings::{Anvil, AnvilInstance};
 
     pub fn spawn_anvil() -> AnvilInstance {
         Anvil::new().block_time(1).try_spawn().unwrap()
-    }
-
-    #[test]
-    fn test_remove_inner_parentheses() {
-        assert_eq!(remove_inner_parentheses("set(uint256 x)"), "set(uint256 x)");
-        assert_eq!(
-            remove_inner_parentheses("set((uint256) x)"),
-            "set(uint256 x)"
-        );
-        assert_eq!(
-            remove_inner_parentheses("set((uint256, uint256) x)"),
-            "set(uint256, uint256 x)"
-        );
-        assert_eq!(
-            remove_inner_parentheses("set((uint256, (uint256, uint256)) x)"),
-            "set(uint256, uint256, uint256 x)"
-        );
     }
 }
