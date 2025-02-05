2640fa0a2ebc1ca8d6383b4e0b11419a55ee5eff
Dan Cline <6798349+Rjected@users.noreply.github.com>
2025-02-05 18:16:39 -0500

    chore(bundle_provider): rewrite BundleClient to use alloy

--- crates/bundle_provider/src/bundle_provider.rs
+++ crates/bundle_provider/src/bundle_provider.rs
@@ -1,92 +1,43 @@
-use alloy::primitives::{Bytes, B256};
-use jsonrpsee::http_client::HttpClient;
-use jsonrpsee::{core::client::ClientT, rpc_params};
-use serde::{Deserialize, Serialize};
+use alloy::{
+    network::AnyNetwork,
+    primitives::Bytes,
+    providers::{Provider, ProviderBuilder, RootProvider},
+    rpc::types::mev::{EthBundleHash, EthSendBundle},
+    transports::http::{reqwest::IntoUrl, Client, Http},
+};
 
+/// A helper wrapper around a RPC client that can be used to call `eth_sendBundle`.
 #[derive(Debug)]
 pub struct BundleClient {
-    client: HttpClient,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct EthSendBundleResponse {
-    pub bundle_hash: B256,
+    client: RootProvider<Http<Client>, AnyNetwork>,
 }
 
 impl BundleClient {
-    pub fn new(url: impl AsRef<str>) -> Self {
-        let client = HttpClient::builder()
-            .build(url)
-            .expect("failed to connect to RPC provider");
-        Self { client }
+    /// Creates a new [`BundleClient`] with the given URL.
+    pub fn new(url: impl IntoUrl) -> Self {
+        let provider = ProviderBuilder::default().on_http(url.into_url().unwrap());
+        Self { client: provider }
     }
 
+    /// Sends a bundle using `eth_sendBundle`, discarding the response.
     pub async fn send_bundle(&self, bundle: EthSendBundle) -> Result<(), String> {
         // Result contents optional because some endpoints don't return this response
-        let res: Result<Option<EthSendBundleResponse>, _> = self
-            .client
-            .request("eth_sendBundle", rpc_params![bundle])
-            .await;
-        if let Err(e) = res {
-            return Err(format!("Failed to send bundle: {:?}", e));
-        }
+        self.client
+            .raw_request::<EthSendBundle, Option<EthBundleHash>>("eth_sendBundle".into(), bundle)
+            .await
+            .map_err(|e| format!("Failed to send bundle: {:?}", e))?;
 
         Ok(())
     }
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct EthSendBundle {
-    /// A list of hex-encoded signed transactions
-    pub txs: Vec<Bytes>,
-    /// hex-encoded block number for which this bundle is valid
-    #[serde(with = "alloy_serde::quantity")]
-    pub block_number: u64,
-    /// unix timestamp when this bundle becomes active
-    #[serde(
-        default,
-        with = "alloy_serde::quantity::opt",
-        skip_serializing_if = "Option::is_none"
-    )]
-    pub min_timestamp: Option<u64>,
-    /// unix timestamp how long this bundle stays valid
-    #[serde(
-        default,
-        with = "alloy_serde::quantity::opt",
-        skip_serializing_if = "Option::is_none"
-    )]
-    pub max_timestamp: Option<u64>,
-    /// list of hashes of possibly reverting txs
-    #[serde(
-        default
-        // this doesn't work on rbuilder:
-        // , skip_serializing_if = "Vec::is_empty"
-    )]
-    pub reverting_tx_hashes: Vec<B256>,
-    /// UUID that can be used to cancel/replace this bundle
-    #[serde(
-        default,
-        rename = "replacementUuid",
-        skip_serializing_if = "Option::is_none"
-    )]
-    pub replacement_uuid: Option<String>,
-}
-
-impl EthSendBundle {
-    pub fn new_basic(txs: Vec<Bytes>, block_number: u64) -> Self {
-        Self {
-            txs,
-            block_number,
-            min_timestamp: None,
-            max_timestamp: None,
-            reverting_tx_hashes: Vec::new(),
-            replacement_uuid: None,
-        }
-    }
-
-    pub async fn send_to_builder(&self, client: &BundleClient) -> Result<(), String> {
-        client.send_bundle(self.clone()).await
+/// Creates a new bundle with the given transactions and block number, setting the rest of the
+/// fields to default values.
+#[inline]
+pub fn new_basic_bundle(txs: Vec<Bytes>, block_number: u64) -> EthSendBundle {
+    EthSendBundle {
+        txs,
+        block_number,
+        ..Default::default()
     }
 }
--- crates/bundle_provider/src/lib.rs
+++ crates/bundle_provider/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod bundle_provider;
-
-pub use bundle_provider::{BundleClient, EthSendBundle, EthSendBundleResponse};
+pub use bundle_provider::BundleClient;
