3e1303eec3e7e8e73eb8a2e1a9437a73a568d199
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2025-03-11 15:44:51 -0400

    use 1 for prio fee

--- crates/core/src/spammer/util.rs
+++ crates/core/src/spammer/util.rs
@@ -67,7 +67,7 @@ pub mod test {
             ..Default::default()
         };
 
-        complete_tx_request(&mut tx_req, tx_type, gas_price, 0 as u128, 21000, chain_id);
+        complete_tx_request(&mut tx_req, tx_type, gas_price, 1 as u128, 21000, chain_id);
 
         let eth_wallet = EthereumWallet::from(sender.to_owned());
         let tx = tx_req.build(&eth_wallet).await?;
