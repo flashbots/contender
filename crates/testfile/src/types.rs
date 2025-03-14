2463803b0f5f2332629a09164ae56a68ee166724
Brock Smedley <2791467+zeroXbrock@users.noreply.github.com>
2025-03-14 16:59:58 -0400

    move AgentStore & TestConfig utils into respective impls, fix broken test

--- crates/testfile/src/types.rs
+++ crates/testfile/src/types.rs
@@ -18,3 +18,54 @@ pub struct TestConfig {
     /// Function to call in spam txs.
     pub spam: Option<Vec<SpamRequest>>, // TODO: figure out how to implement BundleCallDefinition alongside FunctionCallDefinition
 }
+
+impl TestConfig {
+    pub fn get_create_pools(&self) -> Vec<String> {
+        self.create
+            .to_owned()
+            .unwrap_or_default()
+            .into_iter()
+            .filter_map(|s| s.from_pool)
+            .collect()
+    }
+
+    pub fn get_setup_pools(&self) -> Vec<String> {
+        self.setup
+            .to_owned()
+            .unwrap_or_default()
+            .into_iter()
+            .filter_map(|s| s.from_pool)
+            .collect()
+    }
+
+    /// Gets every instance of a `from_pool` declaration in the spam requests.
+    pub fn get_spam_pools(&self) -> Vec<String> {
+        let mut from_pools = vec![];
+        let spam = self
+            .spam
+            .as_ref()
+            .expect("No spam function calls found in testfile");
+
+        for s in spam {
+            match s {
+                SpamRequest::Tx(fn_call) => {
+                    if let Some(from_pool) = &fn_call.from_pool {
+                        from_pools.push(from_pool.to_owned());
+                    }
+                }
+                SpamRequest::Bundle(bundle) => {
+                    for tx in &bundle.txs {
+                        if let Some(from_pool) = &tx.from_pool {
+                            from_pools.push(from_pool.to_owned());
+                        }
+                    }
+                }
+            }
+        }
+
+        // filter out non-unique pools
+        from_pools.sort();
+        from_pools.dedup();
+        from_pools
+    }
+}
