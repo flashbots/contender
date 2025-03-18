933e4d77ee0489f9f52917c352a8281eb0d68333
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-03-18 15:23:15 -0700

    log request id w/ hash when calling sendRawTransaction

---
+++ crates/core/src/provider.rs
@@ -0,0 +1,79 @@
+use std::{
+    fmt::Debug,
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use alloy::{
+    rpc::json_rpc::{RequestPacket, ResponsePacket},
+    transports::TransportError,
+};
+use eyre::Result;
+use tower::{Layer, Service};
+
+pub struct LoggingLayer;
+
+// Implement tower::Layer for LoggingLayer.
+impl<S> Layer<S> for LoggingLayer {
+    type Service = LoggingService<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        LoggingService { inner }
+    }
+}
+
+// A logging service that wraps an inner service.
+#[derive(Debug, Clone)]
+pub struct LoggingService<S> {
+    inner: S,
+}
+
+// Implement tower::Service for LoggingService.
+impl<S> Service<RequestPacket> for LoggingService<S>
+where
+    // Constraints on the service.
+    S: Service<RequestPacket, Response = ResponsePacket, Error = TransportError>,
+    S::Future: Send + 'static,
+    S::Response: Send + 'static + Debug,
+    S::Error: Send + 'static + Debug,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: RequestPacket) -> Self::Future {
+        let mut id = 0;
+        match &req {
+            RequestPacket::Single(inner_req) => {
+                if inner_req.method() == "eth_sendRawTransaction" {
+                    id = inner_req.id().as_number().unwrap_or_default();
+                }
+            }
+            RequestPacket::Batch(_) => {}
+        }
+
+        let fut = self.inner.call(req);
+
+        Box::pin(async move {
+            let res = fut.await;
+            if id != 0 {
+                if let Ok(res) = &res {
+                    match res {
+                        ResponsePacket::Single(inner_res) => {
+                            if let Some(payload) = inner_res.payload.as_success() {
+                                println!("tx delivered. hash: {}, id: {id}", payload.get());
+                            }
+                        }
+                        ResponsePacket::Batch(_) => {}
+                    }
+                }
+            }
+            res
+        })
+    }
+}
