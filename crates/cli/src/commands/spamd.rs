344f0d9d94b420d811f1c07cc44131f503405d32
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-03-28 16:55:24 -0700

    handle CTRL-C in spamd more gracefully; allow result collection to finish

--- crates/cli/src/commands/spamd.rs
+++ crates/cli/src/commands/spamd.rs
@@ -19,9 +19,6 @@ pub async fn spamd(
     gen_report: bool,
     time_limit: Option<u64>,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    // collects all run IDs for reporting
-    let (run_id_sender, mut run_id_receiver) = tokio::sync::mpsc::channel::<u64>(1000);
-
     let SpamCommandArgs {
         testfile,
         rpc_url,
@@ -54,71 +51,61 @@ pub async fn spamd(
         }
     });
 
-    // async-safe handle
-    let is_finished = finished.clone();
-
     let InitializedScenario {
         mut scenario,
         rpc_client,
     } = init_scenario(db, &args).await?;
 
-    // runs spam command in an async loop; in closure for tokio::select to handle CTRL-C
-    let spam_loop = || async move {
-        loop {
-            if is_finished.load(Ordering::SeqCst) {
-                println!("Spam loop finished");
-                break;
-            }
-            let args = SpamCommandArgs {
-                testfile: testfile.clone(),
-                rpc_url: rpc.to_owned(),
-                builder_url: builder_url.clone(),
-                txs_per_block: *txs_per_block,
-                txs_per_second: *txs_per_second,
-                duration: *duration,
-                seed: seed.clone(),
-                private_keys: private_keys.clone(),
-                disable_reporting: *disable_reporting,
-                min_balance: min_balance.clone(),
-                tx_type: *tx_type,
-                gas_price_percent_add: *gas_price_percent_add,
-            };
-            let db = db.clone();
-            let spam_res = commands::spam(&db, args, &mut scenario, &rpc_client).await;
-            if let Err(e) = spam_res {
-                println!("spam failed: {:?}", e);
-            } else {
-                println!("spam batch completed");
-                let run_id = spam_res.expect("spam");
-                if let Some(run_id) = run_id {
-                    // run_ids.push(run_id);
-                    run_id_sender
-                        .send(run_id)
-                        .await
-                        .expect("failed to send run ID");
-                }
-            }
+    // collects run IDs from the spam command
+    let mut run_ids = vec![];
+
+    // if CTRL-C signal is received, set `finished` to true
+    let is_finished = finished.clone();
+    tokio::task::spawn(async move {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to listen for CTRL-C");
+        println!(
+            "CTRL-C received. Spam daemon will shut down as soon as current batch finishes..."
+        );
+        is_finished.store(true, Ordering::SeqCst);
+    });
+
+    // runs spam command in a loop
+    loop {
+        if finished.load(Ordering::SeqCst) {
+            println!("Spam loop finished");
+            break;
         }
-
-        Ok::<_, ContenderError>(())
-    };
-
-    tokio::select! {
-        _ = spam_loop() => {},
-        _ = tokio::signal::ctrl_c() => {
-            println!("CTRL-C received, stopping spam daemon...");
+        let args = SpamCommandArgs {
+            testfile: testfile.clone(),
+            rpc_url: rpc.to_owned(),
+            builder_url: builder_url.clone(),
+            txs_per_block: *txs_per_block,
+            txs_per_second: *txs_per_second,
+            duration: *duration,
+            seed: seed.clone(),
+            private_keys: private_keys.clone(),
+            disable_reporting: *disable_reporting,
+            min_balance: min_balance.clone(),
+            tx_type: *tx_type,
+            gas_price_percent_add: *gas_price_percent_add,
+        };
+        let db = db.clone();
+        let spam_res = commands::spam(&db, args, &mut scenario, &rpc_client).await;
+        if let Err(e) = spam_res {
+            println!("spam failed: {:?}", e);
+        } else {
+            println!("spam batch completed");
+            let run_id = spam_res.expect("spam");
+            if let Some(run_id) = run_id {
+                run_ids.push(run_id);
+            }
         }
     }
 
-    run_id_receiver.close();
-
     // generate a report if requested; in closure for tokio::select to handle CTRL-C
     let run_report = || async move {
-        let mut run_ids = vec![];
-        while let Some(run_id) = run_id_receiver.recv().await {
-            run_ids.push(run_id);
-        }
-
         if gen_report {
             if run_ids.is_empty() {
                 println!("No runs found, exiting.");
