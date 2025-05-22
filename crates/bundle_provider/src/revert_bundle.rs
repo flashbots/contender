91fcc29bb447cdf4ce7760e3af8657636cf530f6
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-05-22 10:20:28 -0700

    remove outdated param

--- crates/bundle_provider/src/revert_bundle.rs
+++ crates/bundle_provider/src/revert_bundle.rs
@@ -7,8 +7,8 @@ use crate::bundle::Bundle;
 pub struct RevertProtectBundle {
     #[serde(rename = "txs")]
     transactions: Vec<Bytes>,
-    block_number_min: Option<u64>,
-    block_number_max: Option<u64>,
+    #[serde(rename = "maxBlockNumber")]
+    pub block_number_max: Option<u64>,
 }
 
 impl RevertProtectBundle {
@@ -19,15 +19,6 @@ impl RevertProtectBundle {
     pub fn with_txs(self, transactions: Vec<Bytes>) -> Self {
         Self {
             transactions,
-            block_number_min: self.block_number_min,
-            block_number_max: self.block_number_max,
-        }
-    }
-
-    pub fn with_block_min(self, min_block: u64) -> Self {
-        Self {
-            transactions: self.transactions,
-            block_number_min: Some(min_block),
             block_number_max: self.block_number_max,
         }
     }
@@ -35,7 +26,6 @@ impl RevertProtectBundle {
     pub fn with_block_max(self, max_block: u64) -> Self {
         Self {
             transactions: self.transactions,
-            block_number_min: self.block_number_min,
             block_number_max: Some(max_block),
         }
     }
