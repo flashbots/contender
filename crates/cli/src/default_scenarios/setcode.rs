f1623eec72322e32e3212242979d658d5fbfb9dd
zeroXbrock <2791467+zeroXbrock@users.noreply.github.com>
2025-08-06 17:17:00 -0700

    setCode: warn instead of error when default args/sig used w/ custom contract

--- crates/cli/src/default_scenarios/setcode.rs
+++ crates/cli/src/default_scenarios/setcode.rs
@@ -1,8 +1,6 @@
-use contender_core::{
-    error::ContenderError,
-    generator::{types::SpamRequest, CreateDefinition, FunctionCallDefinition},
-};
+use contender_core::generator::{types::SpamRequest, CreateDefinition, FunctionCallDefinition};
 use contender_testfile::TestConfig;
+use tracing::warn;
 
 use crate::{
     default_scenarios::{
@@ -58,30 +56,21 @@ impl SetCodeArgs {
         cli_args: SetCodeCliArgs,
         signer_address: String,
     ) -> contender_core::Result<Self> {
-        println!("signer_address: {signer_address}");
+        const DEFAULT_SIG: &str = "execute((address,uint256,bytes)[])";
+        const DEFAULT_ARGS: &str = "[(0x{Counter},0,0xd09de08a)]";
         if cli_args.contract_address.is_some() {
             // require signature & args to be provided, else error
             if cli_args.args.is_none() || cli_args.signature.is_none() {
-                let addr_flag = bold("--address");
-                let args_flag = bold("--args");
-                let sig_flag = bold("--sig");
-                return Err(ContenderError::SpamError(
-                    "invalid arguments:",
-                    Some(format!(
-                        "both {args_flag} and {sig_flag} must be provided with {addr_flag}"
-                    )),
-                ));
+                warn!(
+                    "No args or signature provided, using defaults: {}",
+                    bold(format!("--sig \"{DEFAULT_SIG}\" --args \"{DEFAULT_ARGS}\""))
+                )
             }
         }
 
         // 0xd09de08a is the function signature for `increment()` (which we'll call on the Counter contract)
-
-        let signature = cli_args
-            .signature
-            .unwrap_or("execute((address,uint256,bytes)[])".to_owned());
-        let args = cli_args
-            .args
-            .unwrap_or(vec!["[(0x{Counter},0,0xd09de08a)]".to_owned()]);
+        let signature = cli_args.signature.unwrap_or(DEFAULT_SIG.to_owned());
+        let args = cli_args.args.unwrap_or(vec![DEFAULT_ARGS.to_owned()]);
 
         Ok(Self {
             args,
