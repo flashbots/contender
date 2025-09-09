bcb1eca0af32d10b82e30b3a091a30b4e6202fbc
brock 🤖⚡ <2791467+zeroXbrock@users.noreply.github.com>
2025-09-09 15:14:12 -0700

    fork target RPC URL with anvil on estimate_setup_cost (#332)
    * fork target RPC URL with anvil on estimate_setup_cost
    
    * only fork target node in anvil if not running a test
    
    * chore: clippy
    
    * update release script to make annoted tags

--- crates/core/src/test_scenario.rs
+++ crates/core/src/test_scenario.rs
@@ -296,16 +296,19 @@ where
 "
         );
         // start anvil with dev accounts holding 1M eth
-        let anvil = Anvil::new()
-            .args(["--balance", "1000000"])
-            .try_spawn()
+        let mut anvil = Anvil::new().args(["--balance", "1000000"]);
+        if !cfg!(test) {
+            anvil = anvil.fork(self.rpc_url.to_string());
+        }
+        let anvil = anvil.try_spawn()
             .map_err(|e| {
                 if e.to_string().to_lowercase().contains("no such file") {
-                    ContenderError::SetupError("failed to spawn anvil. You may need to install foundry (https://book.getfoundry.sh/getting-started/installation).", None)
+                ContenderError::SetupError("failed to spawn anvil. You may need to install foundry (https://book.getfoundry.sh/getting-started/installation).", None)
                 } else {
-                    ContenderError::with_err(e, "failed to spawn anvil.")
+                ContenderError::with_err(e, "failed to spawn anvil.")
                 }
             })?;
+
         let admin_signer = LocalSigner::from_str(
             "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
         )
